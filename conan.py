ab6c345e69c900bbee2c1c98f821697c1702bf3d
Spencer Fraint <spencer@intentionet.com>
2017-12-06 11:40:30 -0800

    Started Conan and added sensitive line removal

---
+++ conan.py
@@ -0,0 +1,56 @@
+from sensitive_item_removal import *
+import argparse
+import os
+import logging
+
+def anonymize_files_in_dir(input_dir_path, output_dir_path, compiled_regexes=None):
+    for cur_dir, dir_list, file_list in os.walk(input_dir_path):
+        logging.debug("Input dir " + cur_dir)
+        for f in file_list:
+            if f.endswith(".cfg"):
+                logging.info("Anonymizing " + f)
+                anonymize_file(input_dir_path + "/" + f,
+                                output_dir_path + "/" + f,
+                                compiled_regexes)
+        break
+    return
+
+def anonymize_file(filename_in, filename_out, compiled_regexes=None):
+    logging.debug("File in " + filename_in)
+    logging.debug("File out " + filename_out)
+    with open(filename_out, 'w') as file_out:
+        with open(filename_in, 'r') as file_in:
+            for line in file_in:
+                output_line = line
+                if compiled_regexes is not None:
+                    output_line = replace_matching_item(compiled_regexes, output_line)
+                if line != output_line:
+                    logging.debug("Input line:  " + line.rstrip())
+                    logging.debug("Output line: " + output_line.rstrip())
+                file_out.write(output_line)
+    return
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--inputdirectory', help='Directory containing configurtions to anonymize', default='./configs/')
+    parser.add_argument('-o', '--outputdirectory', help='Directory to place anonymized configs', default='./anon_configs/')
+    parser.add_argument('-p', '--anonymizepassword', help='Anonymize Passwords', action='store_true', default=False)
+    parser.add_argument('-l', '--loglevel', help='Determines what level of logs to display (DEBUG|INFO|WARNING|ERROR|CRITICAL)', default='INFO')
+    options = parser.parse_args()
+    input_dir = options.inputdirectory
+    output_dir = options.outputdirectory
+
+    numeric_level = getattr(logging, options.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % options.loglevel)
+    logging.basicConfig(format='%(levelname)s %(message)s', level=numeric_level)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # If compiled_regexes is None, then passwd removal is skipped in anonymize_files_in_dir
+    compiled_regexes = None
+    if options.anonymizepassword:
+        compiled_regexes = generate_default_sensitive_item_regexes()
+
+    anonymize_files_in_dir(input_dir, output_dir, compiled_regexes)
---
+++ sensitive_item_removal.py
@@ -0,0 +1,63 @@
+import re
+import logging
+
+# Taken from RANCID password scrubbing regexes
+default_password_line_regexes = ['^(\s+password \d+) (.*)',
+            '^(username (\S+) password (\d)) (\S+)(.*)',
+            '^((enable )?(password|passwd)( level \d+)?) (.*)',
+            '^(enable secret) (.*)',
+            '^(username (\S+)(\s.*)? secret) (.*)',
+            '^(username (\S+)(\s.*)? password( \d)?) \S+(.*)',
+            '^(wlccp \S+ username (\S+)(\s.*)? password( \d)?) (\S+)(.*)',
+            '^( set session-key (in|out)bound ah \d+) (.*)',
+            '^( set session-key (in|out)bound esp \d+ (authenticator|cypher)) (.*)',
+            '^((\s*)password) (.*)',
+            '^((\s*)secret) (.*)',
+            '^\s*(.*?neighbor.*?) (\S*) password (.*)',
+            '^(\s*ppp .* hostname) .*(.*)',
+            '^(\s*ppp .* password \d) .*(.*)',
+            '^(ip ftp password) (.*)',
+            '^( ip ospf authentication-key) (.*)',
+            '^(\s+isis password) (\S+)(.*)',
+            '^\s+(domain-password|area-password) (\S+)(\s?(.*))',
+            '^( ip ospf message-digest-key \d+ md5) (.*)',
+            '^(  message-digest-key \d+ md5 (7|encrypted)) (.*)',
+            '^((crypto )?isakmp key( \d)?) \S+ (.*)',
+            '^(\s+standby \d+ authentication) (.*)',
+            '^(\s+key-string \d?)(.*)',
+            '^( l2tp tunnel \S+ password)(.*)',
+            '^( digest secret(\s7)?) (.*)',
+            '^(vpdn username (\S+) password)(.*)',
+            '^(( ikev2)? (local|remote)-authentication pre-shared-key) (.*)',
+            '^(( ikev1)?( pre-shared-key | key |\s?failover key )(ascii-text |hexadecimal )?).*(.*)',
+            '(\s+ldap-login-password) \S+(.*)',
+            '^\s+(wpa-psk ascii|hex \d) (.*)',
+            '^( cable shared-secret) (.*)',
+            '^((tacacs|radius)-server\s(\w*[-\s(\s\S+])*\s?key) (\d )?\S+(.*)',
+            '^(ntp authentication-key \d+ md5) (.*)',
+            '^(syscon password) (\S*)(.*)',
+            '^(snmp-server user( \S+)+ (auth (md5|sha))) (\S+)(.*)',
+            # These are from JUNOS
+            '(\s*authentication-key )[^ ;]+(.*)',
+            '(\s*md5 \d+ key )[^ ;]+(.*)',
+            '(\s*hello-authentication-key )[^ ;]+(.*)',
+            '^(.*\s(secret|simple-password) )[^ ;]+(.*)',
+            '(\s+encrypted-password )[^ ;]+(.*)',
+            '(\s+ssh-(rsa|dsa) )\"(.*)',
+            '^\s+((pre-shared-|)key (ascii-text|hexadecimal)) [^ ;]+(.*)']
+# Taken from RANCID community scrubbing regexes
+default_community_line_regexes = ['^((snmp-server .*community)( [08])?) (\S+)(.*)',
+            # TODO: confirm this catches all community possibilities for snmp-server
+            '^(snmp-server host (\S+)( informs| traps| version (1|2c|3 (\S+))| vrf (\S+))*) (\S+)(.*)',
+            # TODO: see if we need to make the occurence of snmp keyword optional for Juniper
+            '^(\s?snmp( \S+)* (community|trap-group)) [^ ;]+(.*)']
+
+def generate_default_sensitive_item_regexes():
+    return [(re.compile(line)) for line in default_password_line_regexes + default_community_line_regexes]
+
+def replace_matching_item(compiled_regexes, input_line):
+    for compiled_regex in compiled_regexes:
+        if compiled_regex.match(input_line) is not None:
+            logging.debug("Match found on " + input_line.rstrip())
+            return "! Sensitive line SCRUBBED\n"
+    return input_line
