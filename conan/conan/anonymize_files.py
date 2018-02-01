81caf416e2d4d77c85110a323c11fb061f9b9030
Daniel Halperin <dan@intentionet.com>
2018-01-31 17:11:36 -0800

    Reuse anonymizers -- good catch

--- conan/conan/anonymize_files.py
+++ conan/conan/anonymize_files.py
@@ -34,14 +34,22 @@ def anonymize_files_in_dir(input_dir_path, output_dir_path, anon_pwd, anon_ip,
         pwd_lookup = {}
     if sensitive_words is not None:
         sensitive_word_regexes = generate_sensitive_word_regexes(sensitive_words)
+    if anon_ip or undo_ip_anon:
+        anonymizer4 = IpAnonymizer(salt)
+        anonymizer6 = IpV6Anonymizer(salt)
 
     for file_name in os.listdir(input_dir_path):
         input_file = os.path.join(input_dir_path, file_name)
         output_file = os.path.join(output_dir_path, file_name)
         if os.path.isfile(input_file) and not file_name.startswith('.'):
             logging.info("Anonymizing {}".format(file_name))
-            anonymize_file(input_file, output_file, salt, compiled_regexes,
-                           anon_ip, pwd_lookup, sensitive_word_regexes, undo_ip_anon)
+            anonymize_file(input_file, output_file, salt,
+                           compiled_regexes=compiled_regexes,
+                           pwd_lookup=pwd_lookup,
+                           sensitive_word_regexes=sensitive_word_regexes,
+                           undo_ip_anon=undo_ip_anon,
+                           anonymizer4=anonymizer4,
+                           anonymizer6=anonymizer6)
 
     if dumpfile is not None:
         with open(dumpfile, 'w') as f_out:
@@ -49,14 +57,13 @@ def anonymize_files_in_dir(input_dir_path, output_dir_path, anon_pwd, anon_ip,
 
 
 def anonymize_file(filename_in, filename_out, salt, compiled_regexes=None,
-                   anon_ip=False, pwd_lookup=None, sensitive_word_regexes=None, undo_ip_anon=False):
+                   anonymizer4=None, anonymizer6=None, pwd_lookup=None,
+                   sensitive_word_regexes=None, undo_ip_anon=False):
     """Anonymize contents of input file and save to the output file.
 
     This only applies sensitive line removal if compiled_regexes and pwd_lookup
     are not None.  This only applies ip anonymization if anonymizer is not None.
     """
-    anonymizer4 = IpAnonymizer(salt)
-    anonymizer6 = IpV6Anonymizer(salt)
     logging.debug("File in  {}".format(filename_in))
     logging.debug("File out {}".format(filename_out))
     with open(filename_out, 'w') as f_out, open(filename_in, 'r') as f_in:
@@ -65,9 +72,11 @@ def anonymize_file(filename_in, filename_out, salt, compiled_regexes=None,
             if compiled_regexes is not None and pwd_lookup is not None:
                 output_line = replace_matching_item(compiled_regexes,
                                                     output_line, pwd_lookup)
-            if anon_ip:
-                output_line = anonymize_ip_addr(anonymizer4, output_line, undo_ip_anon)
+
+            if anonymizer6 is not None:
                 output_line = anonymize_ip_addr(anonymizer6, output_line, undo_ip_anon)
+            if anonymizer4 is not None:
+                    output_line = anonymize_ip_addr(anonymizer4, output_line, undo_ip_anon)
 
             if sensitive_word_regexes is not None:
                 output_line = anonymize_sensitive_words(sensitive_word_regexes,
