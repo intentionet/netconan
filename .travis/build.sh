0ddc55bc08c92cd5c6fc26c119fa41c7b23d2193
Victor Heorhiadi <progwriter@users.noreply.github.com>
2018-02-08 18:09:48 -0800

    Update flake config (#40)
    * Moving flake configs; ignoring "docstring mood"

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,10 +7,8 @@ if [ -n "$TRAVIS_BUILD_DIR" ]; then
 fi
 
 echo -e "\n  ..... Running flake8 on netconan to check style and docstrings"
-flake8 netconan --ignore=E501
-
-echo -e "\n  ..... Running flake8 on tests to check style and docstrings"
-flake8 tests --ignore=E501
+# Configuration for flake8 is taken from setup.cfg
+flake8
 
 echo -e "\n  ..... Running unit tests with pytest"
 python setup.py test
