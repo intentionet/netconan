072c1e77a27b6cdd55646581d49f1713d38af8cf
Dan Halperin <dhalperi@users.noreply.github.com>
2018-03-12 13:52:21 -0700

    Add coverage data to netconan (#52)
    * Add coverage data to netconan

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
 
     # List pytest requirements for running unit tests
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'requests_mock'],
+    tests_require=['pytest', 'pytest-cov', 'requests_mock'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
