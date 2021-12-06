d3a2845e73a92c717e0f4e9fcaab8caadbb7f0bd
Dan Halperin <dhalperi@users.noreply.github.com>
2021-12-06 14:45:51 -0800

    netconan: require python 3.7+ (#174)

--- setup.py
+++ setup.py
@@ -47,6 +47,26 @@ setup(
     author_email="netconan-dev@intentionet.com",
     # Choose your license
     license="Apache License 2.0",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: Apache Software License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     # What does your project relate to?
     keywords="network configuration anonymizer",
     # You can just specify the packages manually here if your project is
@@ -62,12 +82,7 @@ setup(
     install_requires=[
         "configargparse<1.0.0",
         "bidict<1.0.0",
-        # Only use enum34 for Python older than 3.4
-        'enum34<2.0.0; python_version < "3.4"',
-        # ipaddress is in the Python standard library in 3.3+
-        'ipaddress<2.0.0; python_version < "3.3"',
         "passlib<2.0.0",
-        "six<2.0.0",
     ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
